8d9b00ab14fd35a3f08dc26dcdf8de95f9720e76
Duwaragie <duwaragie22@gmail.com>
2025-07-07 22:58:40 +0530

    techicons fixed which border

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -27,25 +27,29 @@ const InterviewDetails = async ({ params }: RouteParams) => {
 
   return (
     <>
-      <div className="flex flex-row gap-4 justify-between">
-        <div className="flex flex-row gap-4 items-center max-sm:flex-col">
-          <div className="flex flex-row gap-4 items-center">
-            <Image
-              src={getRandomInterviewCover()}
-              alt="cover-image"
-              width={40}
-              height={40}
-              className="rounded-full object-cover size-[40px]"
-            />
-            <h3 className="capitalize">{interview.role} Interview</h3>
+      <div className="light-teal-gradient rounded-lg p-0.5">
+        <div className="bg-background rounded-lg p-6">
+          <div className="flex flex-row gap-4 justify-between">
+            <div className="flex flex-row gap-4 items-center max-sm:flex-col">
+              <div className="flex flex-row gap-4 items-center">
+                <Image
+                  src={getRandomInterviewCover()}
+                  alt="cover-image"
+                  width={40}
+                  height={40}
+                  className="rounded-full object-cover size-[40px]"
+                />
+                <h3 className="capitalize text-primary" >{interview.role} Interview</h3>
+              </div>
+
+              <DisplayTechIcons techStack={interview.techstack} />
+            </div>
+
+            <p className="bg-surface dark:bg-transparent dark:text-white px-4 py-2 rounded-lg h-fit border border-border">
+              {interview.type}
+            </p>
           </div>
-
-          <DisplayTechIcons techStack={interview.techstack} />
         </div>
-
-        <p className="bg-surface px-4 py-2 rounded-lg h-fit border border-border">
-          {interview.type}
-        </p>
       </div>
 
       <Agent
--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -9,8 +9,8 @@ const DisplayTechIcons = async ({techStack} : TechIconProps) => {
 
   return (
     <div className='flex flex-row'>{techIcons.slice(0, 3).map(({tech,url}, index) => (
-      <div key= {tech} className={cn('relative group bg-dark-300 rounded-full p-2 flex-center',index >= 1 && '-ml-3')}>
-        <span className='tech-tooltip'>{tech}</span>
+      <div key= {tech} className={cn('relative group bg-neutral-300 dark:bg-neutral-800 border border-neutral-300 dark:border-neutral-600 rounded-full p-2 flex-center shadow-sm',index >= 1 && '-ml-3')}>
+        <span className='tech-tooltip bg-black text-white'>{tech}</span>
         <Image
           src={url}
           alt={tech}
